e1e4b697f284e4d7186b3e5ed9c17ceb96e8ac31
Crystal EvenRam <crystalevenram@Crystals-Air.attlocal.net>
2024-01-20 17:03:40 -0600

    created forms, submit button and table

---
+++ script.js
@@ -0,0 +1,44 @@
+onReady();
+
+function onReady(){
+console.log('js is working!!!')
+}
+
+function handleSubmit(event) {
+
+    event.preventDefault();
+        let firstNameElement = document.getElementById('firstnameInput');
+        let lastNameElement= document.getElementById('lastnameInput');
+        let idElement= document.getElementById('idInput');
+        let titleElement= document.getElementById('titleInput');
+        let annualSalaryElement= document.getElementById('annualSalaryInput');
+
+        let inputContainer = document.querySelector('tbody');
+        
+        inputContainer.innerHTML += `
+
+ <td>
+        ${firstNameElement.value}
+        </td>
+        <td>
+        ${lastNameElement.value}
+        </td>
+        <td>
+        ${idElement.value}
+        </td>
+        <td>
+        ${titleElement.value}
+        </td>
+        <td>
+        ${annualSalaryElement.value}
+        </td>
+        `;
+    
+        firstNameElement.value = '';
+        lastNameElement.value = '';
+        idElement.value = '';
+        titleElement.value = '';
+        annualSalaryElement.value = '';
+    
+    };
+
